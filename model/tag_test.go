96c526510b9031c4cec35f2494edeb8152ce5327
Matt Simons <mattsimons@ntlworld.com>
2023-01-27 09:06:41 +0000

    remove tag focus

--- model/tag_test.go
+++ model/tag_test.go
@@ -10,10 +10,10 @@ var _ = Describe("Tag filtering", func() {
 	When("Filtering for a specific tag", func() {
 		f := Filter{"tag", true}
 
-		It("should pass when it finds that tag", Focus, func() {
+		It("should pass when it finds that tag", func() {
 			Expect(f.filters(Tag{"tag"})).Should(BeTrue())
 		})
-		It("should fail when it does not find that tag", Focus, func() {
+		It("should fail when it does not find that tag", func() {
 			Expect(f.filters(Tag{"other"})).Should(BeFalse())
 		})
 	})
@@ -21,10 +21,10 @@ var _ = Describe("Tag filtering", func() {
 	When("Filtering out a specific tag", func() {
 		f := Filter{"tag", false}
 
-		It("should fail when it finds that tag", Focus, func() {
+		It("should fail when it finds that tag", func() {
 			Expect(f.filters(Tag{"tag"})).Should(BeFalse())
 		})
-		It("should pass when it does not find that tag", Focus, func() {
+		It("should pass when it does not find that tag", func() {
 			Expect(f.filters(Tag{"other"})).Should(BeTrue())
 		})
 	})
